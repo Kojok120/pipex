7ef301146bd17cb74a110f7916ad6e7af0ff1dde
Kojok120 <kojokamo120@gmail.com>
2025-01-16 11:16:42 +0900

    テストケースを追加

--- test_macOS.sh
+++ test_macOS.sh
@@ -1,4 +1,4 @@
-echo "-------- Mandatory Error Output1 No input file-----------"
+echo "-------- Mandatory Error1 No input file-----------"
 #期待される本家の挙動
 echo "Expected:"
 <input_no_exist.txt grep a1 | wc -w > output.txt
@@ -13,10 +13,10 @@ echo "\\npipex:"
 ./pipex input_no_exist.txt "grep a1" "wc -w" output.txt
 cat output.txt
 
-echo "-------- Mandatory Error Output2 No command-----------"
+echo "\\n-------- Mandatory Error2 No command-----------"
 #期待される本家の挙動
 echo "Expected:"
-<input.txt  > output.txt
+<input.txt  "" "" > output.txt
 
 #output.txtを削除
 rm -f output.txt
@@ -27,7 +27,20 @@ echo "\\npipex:"
 ./pipex input.txt "" "" output.txt
 cat output.txt
 
-echo "-------- Mandatory Correct Output1 -----------"
+
+echo "\\n-------- Mandatory Error3 Invalid argument number-----------"
+#オリジナルpipexの挙動のみ
+echo "\\npipex: no command"
+./pipex input_no_exist.txt output.txt
+cat output.txt
+
+echo "\\n\\npipex: argument number is 3"
+echo "Arguments: input_no_exist.txt  "grep a1"  output.txt\\n"
+./pipex input_no_exist.txt "grep a1" output.txt
+cat output.txt
+
+
+echo "\\n-------- Mandatory Correct Output1 -----------"
 #期待される本家の挙動
 echo "Expected:"
 <input.txt grep a1 | wc -w > output.txt
@@ -42,7 +55,7 @@ echo "\\npipex:"
 ./pipex input.txt "grep a1" "wc -w" output.txt
 cat output.txt
 
-echo "-------- Mandatory Correct Output2 -----------"
+echo "\\n-------- Mandatory Correct Output2 -----------"
 #期待される本家の挙動
 echo "Expected:"
 <input.txt ls -l | wc -l > output.txt
@@ -55,4 +68,20 @@ touch output.txt
 #オリジナルpipexの挙動
 echo "\\npipex:"
 ./pipex input.txt "ls -l" "wc -l" output.txt
+cat output.txt
+
+
+echo "\\n-------- Mandatory Correct Output3 No output file-----------"
+#期待される本家の挙動
+echo "Expected:"
+rm -f output.txt
+<input.txt grep a1 | wc -w > output.txt
+cat output.txt
+
+#output.txtを削除
+rm -f output.txt
+
+#オリジナルpipexの挙動
+echo "\\npipex:"
+./pipex input.txt "grep a1" "wc -w" output.txt
 cat output.txt
--- test_mandatory.sh
+++ test_mandatory.sh
@@ -1,19 +1,4 @@
-echo "-------- Mandatory Error Output1 No input file-----------"
-#期待される本家の挙動
-echo "Expected:"
-<input_no_exist.txt grep a1 | wc -w > output.txt
-cat output.txt
-
-#output.txtを削除
-rm -f output.txt
-touch output.txt
-
-#オリジナルpipexの挙動
-echo "\\npipex:"
-valgrind --leak-check=full -q ./pipex input_no_exist.txt "grep a1" "wc -w" output.txt
-cat output.txt
-
-echo "-------- Mandatory Error Output2 No command-----------"
+echo "-------- Mandatory Error1 No command-----------"
 #期待される本家の挙動
 echo "Expected:"
 <input.txt  > output.txt
@@ -26,6 +11,33 @@ touch output.txt
 echo "\\npipex:"
 valgrind --leak-check=full -q ./pipex input.txt "" "" output.txt
 cat output.txt
+
+
+echo "-------- Mandatory Error2 No command-----------"
+#期待される本家の挙動
+echo "Expected:"
+<input.txt  > output.txt
+
+#output.txtを削除
+rm -f output.txt
+touch output.txt
+
+#オリジナルpipexの挙動
+echo "\\npipex:"
+valgrind --leak-check=full -q ./pipex input.txt "" "" output.txt
+cat output.txt
+
+
+echo "-------- Mandatory Error3 Invalid argument number-----------"
+#オリジナルpipexの挙動のみ
+echo "\\npipex: no command"
+valgrind --leak-check=full -q ./pipex input_no_exist.txt output.txt
+cat output.txt
+
+echo "\\npipex: argument number is 3"
+valgrind --leak-check=full -q ./pipex input_no_exist.txt "grep a1" output.txt
+cat output.txt
+
 
 echo "-------- Mandatory Correct Output1 -----------"
 #期待される本家の挙動
@@ -42,6 +54,7 @@ echo "\\npipex:"
 valgrind --leak-check=full -q ./pipex input.txt "grep a1" "wc -w" output.txt
 cat output.txt
 
+
 echo "-------- Mandatory Correct Output2 -----------"
 #期待される本家の挙動
 echo "Expected:"
@@ -55,4 +68,20 @@ touch output.txt
 #オリジナルpipexの挙動
 echo "\\npipex:"
 valgrind --leak-check=full -q ./pipex input.txt "ls -l" "wc -l" output.txt
+cat output.txt
+
+
+echo "-------- Mandatory Correct Output3 No output file-----------"
+#期待される本家の挙動
+echo "Expected:"
+rm -f output.txt
+<input.txt grep a1 | wc -w > output.txt
+cat output.txt
+
+#output.txtを削除
+rm -f output.txt
+
+#オリジナルpipexの挙動
+echo "\\npipex:"
+valgrind --leak-check=full -q ./pipex input.txt "grep a1" "wc -w" output.txt
 cat output.txt
