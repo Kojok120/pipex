4e78623ff8d6d1346362fe19c87084a97af0a70c
Kojok <kojokamo120@gmail.com>
2025-01-28 14:08:31 +0900

    testケースを追加

--- test_mandatory.sh
+++ test_mandatory.sh
@@ -1,17 +1,8 @@
-echo "-------- Mandatory Error1 No command-----------"
-#期待される本家の挙動
-echo "Expected:"
-<input.txt  > output.txt
-
-#output.txtを削除
-rm -f output.txt
-touch output.txt
-
-#オリジナルpipexの挙動
-echo "\\npipex:"
-valgrind --leak-check=full -q ./pipex input.txt "" "" output.txt
+echo "-------- Mandatory Error1 Invalid argument number-----------"
+#オリジナルpipexの挙動のみ
+echo "\\npipex: argument number is 3"
+valgrind --leak-check=full -q ./pipex input_no_exist.txt "grep a1" output.txt
 cat output.txt
-
 
 echo "-------- Mandatory Error2 No command-----------"
 #期待される本家の挙動
@@ -24,25 +15,59 @@ touch output.txt
 
 #オリジナルpipexの挙動
 echo "\\npipex:"
+valgrind --leak-check=full -q ./pipex input.txt output.txt
+cat output.txt
+
+
+echo "\\n-------- Mandatory Error3 No command-----------"
+#期待される本家の挙動
+echo "Expected:"
+<input.txt  "" | "" > output.txt
+
+#output.txtを削除
+rm -f output.txt
+touch output.txt
+
+#オリジナルpipexの挙動
+echo "\\npipex:"
 valgrind --leak-check=full -q ./pipex input.txt "" "" output.txt
 cat output.txt
 
 
-echo "-------- Mandatory Error3 Invalid argument number-----------"
-#オリジナルpipexの挙動のみ
-echo "\\npipex: no command"
-valgrind --leak-check=full -q ./pipex input_no_exist.txt output.txt
+echo "\\n-------- Mandatory Error4 Invalid Command-----------"
+#期待される本家の挙動
+echo "Expected:"
+<input.txt  grep a1 | jdsifo > output.txt
+
+#output.txtを削除
+rm -f output.txt
+touch output.txt
+
+#オリジナルpipexの挙動
+echo "\\npipex:"
+valgrind --leak-check=full -q ./pipex input.txt "grep a1" "jdsifo" output.txt
 cat output.txt
 
-echo "\\npipex: argument number is 3"
-valgrind --leak-check=full -q ./pipex input_no_exist.txt "grep a1" output.txt
+
+echo "\\n-------- Mandatory Error5 Second is Empty-----------"
+#期待される本家の挙動
+echo "Expected:"
+<input.txt  "cat" | "" > output.txt
+
+#output.txtを削除
+rm -f output.txt
+touch output.txt
+
+#オリジナルpipexの挙動
+echo "\\npipex:"
+valgrind --leak-check=full -q ./pipex input.txt "cat" "" output.txt
 cat output.txt
 
 
 echo "-------- Mandatory Correct Output1 -----------"
 #期待される本家の挙動
 echo "Expected:"
-<input.txt grep a1 | wc -w > output.txt
+<input.txt "cat" | "wc" > output.txt
 cat output.txt
 
 #output.txtを削除
@@ -51,7 +76,7 @@ touch output.txt
 
 #オリジナルpipexの挙動
 echo "\\npipex:"
-valgrind --leak-check=full -q ./pipex input.txt "grep a1" "wc -w" output.txt
+valgrind --leak-check=full -q ./pipex input.txt "cat" "wc" output.txt
 cat output.txt
 
 
